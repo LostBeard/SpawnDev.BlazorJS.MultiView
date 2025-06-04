82bc43783578259dc768dc8e84965a971df139fc
Todd Tanner <todd@spawndev.com>
2025-06-03 21:39:13 -0400

    Add project files.

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# SpawnDev.BlazorJS.MultiView
+
+Tools for rendering 2D+Z, Side by Side, Over Under, and other 3D formats in Blazor WebAssembly applications.
---
+++ SpawnDev.BlazorJS.MultiView.Demo/Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SpawnDev.BlazorJS;
+using SpawnDev.BlazorJS.MultiView.Demo;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddBlazorJSRuntime();
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().BlazorJSRunAsync();
