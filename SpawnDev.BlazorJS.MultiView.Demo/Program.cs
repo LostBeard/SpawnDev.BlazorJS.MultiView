4ca18d4834275f33eeb4e29853fe3ae36b8116d7
Todd Tanner <todd@spawndev.com>
2025-06-06 14:21:11 -0400

    Updated demo to use new Nuget package  SpawnDev.BlazorJS.TransformersJS.DepthAnythingV2

--- SpawnDev.BlazorJS.MultiView.Demo/Program.cs
+++ SpawnDev.BlazorJS.MultiView.Demo/Program.cs
@@ -2,9 +2,15 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SpawnDev.BlazorJS;
 using SpawnDev.BlazorJS.MultiView.Demo;
+using SpawnDev.BlazorJS.TransformersJS.DepthAnythingV2;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddBlazorJSRuntime();
+
+builder.Services.AddDepthAnything(de =>
+{
+    //de.UseHuggingFaceCDN = true; // Use HuggingFace CDN for model files
+});
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
